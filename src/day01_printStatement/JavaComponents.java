ed0d6805673248edeef27635198dfe5c24f40ab8
Kemail-terminal <sulkemref@gmail.com>
2022-08-18 15:18:29 -0500

    day01_printStatement_JavaComponents

---
+++ src/day01_printStatement/JavaComponents.java
@@ -0,0 +1,24 @@
+package day01_printStatement;
+
+public class JavaComponents {
+
+    public static void main(String[] args) {
+
+        System.out.println("JDK: Java Development Kit");
+
+        System.out.println("JRE: Java Runtime Environment");
+
+        System.out.println("JVM: Java Virtual Machine");
+
+
+    }
+
+}
+
+/*
+Print the full form of each of the followings:
+    JDK
+    JRE
+    JVM
+
+ */
