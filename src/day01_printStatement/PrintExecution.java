3567f3b673204bf1793c21d1caf94f82fc81f5dc
Kemail-terminal <sulkemref@gmail.com>
2022-08-18 15:18:37 -0500

    day01_printStatement_PrintExecution

---
+++ src/day01_printStatement/PrintExecution.java
@@ -0,0 +1,37 @@
+package day01_printStatement;
+
+public class PrintExecution {
+
+    public static void main(String[] args) {
+
+        System.out.println("Cydeo");
+        System.out.println();
+        System.out.println("Yuliia");
+        System.out.println("Alena");
+        System.out.println("Lucy");
+        System.out.println("Alexander");
+        System.out.println();
+        System.out.println("Igor");
+        System.out.println("Sumeye");
+        System.out.println("Madina");
+
+
+    }
+
+}
+
+/*
+Write a code to print following outputs in the console:
+
+        Cydeo
+
+        Yuliia
+        Alena
+        Lucy
+        Alexander
+
+        Igor
+        Sumeye
+        Madina
+
+ */
