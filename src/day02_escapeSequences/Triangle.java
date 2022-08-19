a9190ead77851f6876e51a09baa4a213ec3c3c17
Kemail-terminal <sulkemref@gmail.com>
2022-08-19 15:19:30 -0500

    day02_escapeSequences_Triangle

---
+++ src/day02_escapeSequences/Triangle.java
@@ -0,0 +1,27 @@
+package day02_escapeSequences;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t    ^");
+        System.out.println("\t\t\t   / \\");
+        System.out.println("\t\t\t  /   \\");
+        System.out.println("\t\t\t /     \\");
+        System.out.println("\t\t\t/       \\");
+        System.out.println("\t\t\t---------");
+
+
+    }
+
+}
+
+/*
+Create a class called Triangle.java and print:
+                    ^
+                   / \
+                  /   \
+                 /     \
+                /       \
+                ---------
+ */
