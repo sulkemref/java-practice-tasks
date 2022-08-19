47ad8b5942c189c1f504f455f096b52b6bb4f5ca
Kemail-terminal <sulkemref@gmail.com>
2022-08-19 15:18:52 -0500

    day02_escapeSequences_AboutMe

---
+++ src/day02_escapeSequences/AboutMe.java
@@ -0,0 +1,11 @@
+package day02_escapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("\tMy favorite music is \"Rock\" musics. \n\t\t\t\tMy favorite book is \"Data Structure in Java\"");
+
+    }
+
+}
