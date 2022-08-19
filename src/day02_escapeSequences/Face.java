b97159032bb062295bd6687eba4f3f93e3a378b3
Kemail-terminal <sulkemref@gmail.com>
2022-08-19 15:19:24 -0500

    day02_escapeSequences_Face

---
+++ src/day02_escapeSequences/Face.java
@@ -0,0 +1,15 @@
+package day02_escapeSequences;
+
+public class Face {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t  +\"\"\"\"\"+");
+        System.out.println("\t [| O O |]");
+        System.out.println("\t  |  ^  |");
+        System.out.println("\t  | '—' |");
+        System.out.println("\t  +-----+");
+
+    }
+
+}
