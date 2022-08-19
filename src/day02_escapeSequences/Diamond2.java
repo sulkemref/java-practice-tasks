616afc1b9b30ca765b1f113f8ae61ab61e998017
Kemail-terminal <sulkemref@gmail.com>
2022-08-19 15:19:16 -0500

    day02_escapeSequences_Diamond2

---
+++ src/day02_escapeSequences/Diamond2.java
@@ -0,0 +1,35 @@
+package day02_escapeSequences;
+
+public class Diamond2 {
+    public static void main(String[] args) {
+
+        System.out.println("\t    ^");
+        System.out.println("\t   / \\  ");
+        System.out.println("\t  / * \\  ");
+        System.out.println("\t / * * \\ ");
+        System.out.println("\t/ * * * \\");
+        System.out.println("\t--------- ");
+        System.out.println("\t\\ * * * /");
+        System.out.println("\t \\ * * / ");
+        System.out.println("\t  \\ * /  ");
+        System.out.println("\t   \\ /  ");
+        System.out.println("\t    v");
+
+    }
+
+}
+
+/*
+Write a code to print the following output in the console:
+                    ^
+                   / \
+                  / * \
+                 / * * \
+                / * * * \
+                ---------
+                \ * * * /
+                 \ * * /
+                  \ * /
+                   \ /
+                    V
+ */
