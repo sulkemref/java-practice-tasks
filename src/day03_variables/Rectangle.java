fb9f4804817f110f4d42c8c09120530b54f8985c
Kemail-terminal <sulkemref@gmail.com>
2022-08-25 15:19:55 -0500

    day03_variables_Rectangle

---
+++ src/day03_variables/Rectangle.java
@@ -0,0 +1,31 @@
+package day03_variables;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+        double length = 5.5;
+        double width = 10.5;
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+        System.out.println("length = " + length);
+        System.out.println("width = " + width);
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
+
+
+
+
+
+    }
+
+}
+
+/*
+ write a program that can calculate the area & perimeter of any given Rectangle
+						length
+						width
+						area = length * width
+						perimeter =  2 * (length + width)
+ */
