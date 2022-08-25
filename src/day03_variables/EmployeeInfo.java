5dbdd1d68e50a4ed279d89fd057f97221ac9c04b
Kemail-terminal <sulkemref@gmail.com>
2022-08-25 15:19:47 -0500

    day03_variables_EmployeeInfo

---
+++ src/day03_variables/EmployeeInfo.java
@@ -0,0 +1,46 @@
+package day03_variables;
+
+public class EmployeeInfo {
+    public static void main(String[] args) {
+
+        String employeeName = "Shay";
+        int age = 24;
+        char gender = 'F';
+        String company = "Apple Inc";
+        String jobTitle = "SDET";
+        double yearsOfExperience =2.5;
+        int salary = 120000;
+        boolean isFullTime = true;
+        boolean isMarried = false;
+        String employeeId = "A101";
+
+
+        System.out.println("employeeName = " + employeeName);
+        System.out.println("employeeId = " + employeeId);
+        System.out.println("gender = " + gender);
+        System.out.println("age = " + age);
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("company = " + company);
+        System.out.println("yearsOfExperience = " + yearsOfExperience);
+        System.out.println("isFullTime = " + isFullTime);
+        System.out.println("salary = " + salary);
+        System.out.println("isMarried = " + isMarried);
+
+
+    }
+
+}
+/*
+Create a class employeeNamed EmployeeInfo. declare the following variables:
+				1. employeeName (String)
+				2. age (int)
+				3. gender (char)
+				4. company (String)
+				5. jobTitle (String)
+				6. yearsOfExperience (double)
+				7. salary (int)
+				8. isFullTime (boolean)
+				9. isMarried (boolean)
+				10. employeeId (String)
+				11. SSN (String)
+ */
