9002170af7acb4c28593544d701a17c73990021c
Kemail-terminal <sulkemref@gmail.com>
2022-08-26 15:20:37 -0500

    day04_concatenation_GallonsToLitters

---
+++ src/day04_concatenation/GallonsToLitters.java
@@ -0,0 +1,30 @@
+package day04_concatenation;
+
+public class GallonsToLitters {
+
+    public static void main(String[] args) {
+
+        double gallons = 10;
+
+        double litters = gallons * 3.79;
+
+        System.out.println(gallons + " gallons is equal to " + litters + " litters");
+
+    }
+
+}
+
+/*
+Create a class named GallonsToLiters and declare the following variables:
+                gallon
+                liters
+
+    Write a program that can convert the any given number of gallons to liters
+        Ex:
+              gallon = 10
+
+        output:
+              10 gallons is equal to 37.9 litters
+
+           Hint:  1 gal = 3.79 L
+ */
