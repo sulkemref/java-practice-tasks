5f621eadd5a6689e0c69eea4ee3cca4048079503
Kemail-terminal <sulkemref@gmail.com>
2022-09-22 17:39:09 -0500

    day14_forLoop_Shape

---
+++ src/day14_forLoop/Shape.java
@@ -0,0 +1,26 @@
+package day14_forLoop;
+
+public class Shape {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println("* * * * * *");
+        }
+
+    }
+
+}
+
+/*
+Use a loop to print the following shape:
+
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+ */
