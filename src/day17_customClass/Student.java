7e1c7f83995df8c116a182522759edc34f3f60ba
Kemail-terminal <sulkemref@gmail.com>
2022-10-06 19:13:29 -0500

    day18_extraPractices_Carpet

---
+++ src/day17_customClass/Student.java
@@ -0,0 +1,49 @@
+package day17_customClass;
+
+public class Student {
+
+    public String name;
+    public char gender;
+    public int age;
+    public int ID;
+    public char grade;
+
+    public void setInfo(String name, char gender, int age, int ID, char grade) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.ID = ID;
+        this.grade = grade;
+    }
+
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", ID=" + ID +
+                ", grade=" + grade +
+                '}';
+    }
+
+    public void code(){
+        System.out.println(name +" is coding");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+}
+
+/*
+ Create a custom class named Student
+		Attributes:
+			name, gender, age, studentId, grade, isFullTime
+
+		Actions:
+			setInfo(): sets all the fields of the student object
+			toString(): when a car object is passed in print statement,
+					it should display all the information of the student object
+			study()
+ */
