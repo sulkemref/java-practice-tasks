49a27f3968a37da4d755498b1e6b00a963601b69
Kemail-terminal <sulkemref@gmail.com>
2022-10-06 19:14:09 -0500

    day18_extraPractices_HighestFrequency

---
+++ src/day18_extraPractices/HighestFrequency.java
@@ -0,0 +1,58 @@
+package day18_extraPractices;
+
+public class HighestFrequency {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbccccddeeee";
+
+        int highestFrequency = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count > highestFrequency) {
+                highestFrequency = count;
+            }
+
+        }
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == highestFrequency && !result.contains(ch + "")) {
+                result += ch;
+            }
+
+        }
+
+
+        System.out.println(result);
+
+    }
+
+}
+
+/*
+1. Write a program that can return the character that has the highest frequency from a string
+	Ex:
+		str = "aaabbccccddeeee"
+
+		output:
+			c
+			e
+ */
