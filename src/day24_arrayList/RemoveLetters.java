6166ba36f45b8cd631d8a8a03a5baa059e3b1623
Kemail-terminal <sulkemref@gmail.com>
2022-10-27 19:20:52 -0500

    day24_arrayList_RemoveDigits

---
+++ src/day24_arrayList/RemoveLetters.java
@@ -0,0 +1,28 @@
+package day24_arrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveLetters {
+
+    public static void main(String[] args) {
+        ArrayList<Character> list = new ArrayList<>();
+        list.addAll(Arrays.asList('$', 'A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'));
+
+        list.removeIf(p -> Character.isLetter(p));
+
+        System.out.println(list);
+
+    }
+
+}
+
+/*
+3. Write a program that can remove all the letters from an arrayList of characters
+		ex:
+			list = {'$', A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'}
+
+		output:
+			['$',  '1', '2', '@', '!', '3', '4']
+
+ */
