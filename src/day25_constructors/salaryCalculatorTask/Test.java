ca31f58298dca3923b8af8583fe8c0bb85c9fd3e
Kemail-terminal <sulkemref@gmail.com>
2022-10-28 19:22:35 -0500

    day25_constructors_salaryCalculatorTask

---
+++ src/day25_constructors/salaryCalculatorTask/Test.java
@@ -0,0 +1,11 @@
+package day25_constructors.salaryCalculatorTask;
+
+public class Test {
+
+    public static void main(String[] args) {
+        SalaryCalculator obj = new SalaryCalculator(65,40,5.4, 25.5);
+
+        System.out.println(obj);
+    }
+
+}
