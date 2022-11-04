5d738ba50853b7cfa4e8954ac0052eadb2de67f3
Kemail-terminal <sulkemref@gmail.com>
2022-11-03 19:22:58 -0500

    day26_statics_cydeoStudentTask

---
+++ src/day26_statics/circleTask/Test.java
@@ -0,0 +1,21 @@
+package day26_statics.circleTask;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(3.5);
+        Circle circle2 = new Circle(3.5);
+
+        System.out.println(circle1);
+        System.out.println(circle2);
+
+        System.out.println(circle1.pi);
+        System.out.println(circle2.pi);
+
+
+
+    }
+
+
+}
