10780e5749facf09d82d4e914e86697474c63072
Kemail-terminal <sulkemref@gmail.com>
2022-11-04 19:23:46 -0500

    day27_statics_iphoneTask

---
+++ src/day27_statics/iphoneTask/Test.java
@@ -0,0 +1,17 @@
+package day27_statics.iphoneTask;
+
+public class Test {
+    public static void main(String[] args) {
+
+        IPhone iPhone = new IPhone("iphone 12", "Black", 1000, "5.6 inches");
+
+        System.out.println(iPhone);
+
+        iPhone.faceTime(123456789);
+        iPhone.faceTime("gmail@yahoo.com");
+        iPhone.call(123456789);
+        iPhone.text(123456789);
+
+
+    }
+}
