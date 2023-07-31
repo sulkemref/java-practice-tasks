e18731d583483a7f4774177c27a6859f1b444820
Kemail-terminal <sulkemref@gmail.com>
2023-07-31 15:18:20 -0500

    day01_printStatement_GasReceipt

---
+++ src/day01_printStatement/GasReceipt.java
@@ -0,0 +1,26 @@
+package day01_printStatement;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("    MCLEAN STORE");
+        System.out.println();
+        System.out.println("04/29/2022  02:45pm");
+        System.out.println();
+        System.out.println("Gallons:        11.48");
+        System.out.println("Price/gallon: $ 4.15");
+        System.out.println();
+        System.out.println("Fuel total:   $ 47.642");
+
+
+
+    }
+
+}
+
+/*
+1. Write a code to print the following output in the console:
+            Pic is in day01 presentation slide
+ */
